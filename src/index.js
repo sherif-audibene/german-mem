be93bdeb3cfbdecab0bc018428761bb8cfd3001d
Sherif Hamad <sherif.hamad@audibene.de>
2022-06-19 19:31:10 +0200

    First working build

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Words from "./Components/Words";
-import Banner from "./Components/Banner";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Banner />
     <Words />
     <App />
   </React.StrictMode>
